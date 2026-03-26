5ac720f3d7ed6962f43993c41a04a801a5ce7647
Leandro Ludwig Batista <ludwigleandro150@gmail.com>
2026-03-26 16:24:41 -0300

    Add files via upload

---
+++ alugames/js/app.js
@@ -0,0 +1,19 @@
+function alterarStatus(numero) {
+    let jogo = document.getElementById(`game-${numero}`);
+    let imagem = jogo.querySelector(".dashboard__item__img");
+    let botao = jogo.querySelector(".dashboard__item__button");
+
+        if (imagem.classList.contains('dashboard__item__img--rented')) {
+            imagem.classList.remove('dashboard__item__img--rented'); 
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        } else {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.textContent = 'devolver';
+            botao.classList.add('dashboard__item__button--return');
+        }
+
+}
+
+
+
